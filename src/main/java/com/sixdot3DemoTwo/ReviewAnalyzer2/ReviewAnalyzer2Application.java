0cb4732cb4f280ee932faf63fb9b7c9e656939ec
Hector <hectormrg0823@gmail.com>
2021-02-08 11:21:14 -0800

    new files

---
+++ src/main/java/com/sixdot3DemoTwo/ReviewAnalyzer2/ReviewAnalyzer2Application.java
@@ -0,0 +1,13 @@
+package com.sixdot3DemoTwo.ReviewAnalyzer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalyzer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAnalyzer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sixdot3DemoTwo/ReviewAnalyzer2/ReviewAnalyzer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixdot3DemoTwo.ReviewAnalyzer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyzer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
